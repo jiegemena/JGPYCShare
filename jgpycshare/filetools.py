3ea670db07791d7dcaa6cf5666e4f59ff1eaf24f
jiegemena <jiegemena@163.com>
2019-05-30 14:04:07 +0800

    add http json tools

--- jgpycshare/filetools.py
+++ jgpycshare/filetools.py
@@ -13,6 +13,9 @@
 
 import os
 import requests 
+import os.path 
+import shutil 
+import time, datetime
 
 def downfiles(durl,fielpath=None):
     if fielpath is None or len(fielpath) > 0:
@@ -36,3 +39,26 @@ def mkdir(path):
     else:
         print (path+' 目录已存在')
         return False
+
+
+def copydir(oldf,newf):
+    '''
+    - copy dir
+    '''
+    shutil.copy(oldf, newf)
+
+def copyfile(oldf,newf):
+    '''
+    copy file
+    '''
+    for root, dirs, files in os.walk(oldf):
+        filenametmp = newf + root
+        if filenametmp.find('__pycache__') >= 0:
+            continue
+        mkdir(filenametmp)
+        for f in files:
+            shutil.copy(root + '/' + f, filenametmp)
+
+        print('root',root) #当前目录路径  
+        # print('dirs',dirs) #当前路径下所有子目录  
+        print('files',files) #当前路径下所有非目录子文件 
--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ from setuptools import setup, find_packages
 
 setup(name="jgpycshare",
       description="jgpycshare",
-      version="1.8.4",
+      version="1.8.6",
       author="jiegemena",
       author_email="jiegemena@outlook.com",
       packages=find_packages(),
